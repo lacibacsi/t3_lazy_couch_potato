07ac98a601eeb6c5199cf171a3631283abe5269f
lacibacsi <kovaril@gmail.com>
2022-05-03 23:40:37 +0000

    empty stubs

---
+++ script/qlearn.py
@@ -0,0 +1,7 @@
+#! usr/bin/env
+
+'''
+    qlearn implementation -> used in assessment 2 for the Robotics cours
+'''
+
+#TODO
---
+++ script/sarsa.py
@@ -0,0 +1,7 @@
+#! usr/bin/env
+
+'''
+    sarsa implementation -> used in assessment 2 for the Robotics cours
+'''
+
+# TODO
